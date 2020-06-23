4e01779f18fbfc35edb0c394e2167cfe9122e2d7
New Dad <josh_panakkal3122@hotmail.com>
2020-06-23 11:09:47 -0400

    added input tags, styled and rendered.

---
+++ script.js
@@ -0,0 +1,13 @@
+//9 input blocks to type events into 9am-5pm
+    //adding text content to input blocks
+
+//save buttons to save events into local storage and display in input blocks
+    //event listeners
+
+//color coding events to indicate past, present or future
+    //conditional statements which change background color of input fields 
+
+//display the current day at the top of the planner
+
+//standard business time stamps 9am - 5pm
+
