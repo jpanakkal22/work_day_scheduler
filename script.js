a905cc36ddbbe6809aca104f0f9d185ea0e07df8
New Dad <josh_panakkal3122@hotmail.com>
2020-06-26 13:59:22 -0400

    set and get items from local storage

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-//9 input blocks to type events into 9am-5pm
-    //adding text content to input blocks
-
-//save buttons to save events into local storage and display in input blocks
-    //event listeners
-
-//color coding events to indicate past, present or future
-    //conditional statements which change background color of input fields 
-
-//display the current day at the top of the planner
-
-//standard business time stamps 9am - 5p
-
-var m = moment().format('MMMM Do YYYY');
-
+//Moment.js date
+var m = moment();
 $("#currentDay").text(m.toString());    
+console.log(m);
+
+var labelEl = document.querySelector("#time");
+var inputEl = document.querySelector("input");
+var saveButton = document.querySelector("#saveBtn");
+var render = document.querySelector("#render");
+var label = document.querySelector("label");
+
+labelEl.textContent = "11am";
+
+
+colorChange();
+
+
+saveButton.addEventListener("click", function(event){
+    event.preventDefault();
+    var button = event.target;
+      
+    renderInput();   
+        
+    if(inputEl === ""){
+        return;
+    }
+    
+    var text = inputEl.value;
+    localStorage.setItem("text", text);
+    var save = localStorage.getItem("text");
+    inputEl.textContent = save;
+
+})
+
+function renderInput(){
+    var renderedText = localStorage.getItem("text");
+    render.textContent = renderedText;
+}
+
+function colorChange(){
+    if(labelEl.textContent === "9am"){
+        render .setAttribute("class", "past");
+    }
+    else if(labelEl.textContent === "10am"){
+        render.setAttribute("class", "present");
+    }
+    else {
+        render.setAttribute("class", "future")
+    }
+   
+   
+}
+
+
+
+
+
     
