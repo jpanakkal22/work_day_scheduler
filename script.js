9ae95d00027750848c399608e297d12a66bcde35
New Dad <josh_panakkal3122@hotmail.com>
2020-06-26 23:12:16 -0400

    added data attributes to buttons and event delegation

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
 //Moment.js date
 var m = moment();
 $("#currentDay").text(m.toString());    
-console.log(m);
+
 
 var labelEl = $("#time");
 var inputEl = $("input");
-var saveButton = $("#saveBtn");
+var saveButton = $(".saveBtn");
 var render = $("#render");
 var label = $("label");
 
-labelEl.textContent = "11am";
+
 
 renderInput()
-// colorChange();
 
 saveButton.on("click", function(event){
     event.preventDefault();
-    var button = event.target;      
-     
-        
+           
     if(inputEl === ""){
         return;
-        }
+    }
+
+    var currentbutton = $(this).attr("data-time")
+    console.log(currentbutton);    
     
     var text = inputEl.val();
     localStorage.setItem("text", text);
-    // renderInput(); 
-    console.log(text);
-    
+        
 })
 
 function renderInput(){
     var renderedText = localStorage.getItem("text");
-    // render.textContent = renderedText;
     inputEl.val(renderedText);
 }
 
